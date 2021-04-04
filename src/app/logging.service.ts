e9fe4d7e7cb26cb634df4218c88c0b32040928a0
erwin <winn92091229@gmail.com>
2021-04-04 13:52:16 -0400

    After creating logging services and injecting it

---
+++ src/app/logging.service.ts
@@ -0,0 +1,5 @@
+export class LogginService {
+  logStatusChange(status: string) {
+    console.log("A server status changed, new status: " + status);
+  }
+}
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,23 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from "@angular/core";
+import { LogginService } from "../logging.service";
 
 @Component({
-  selector: 'app-new-account',
-  templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  selector: "app-new-account",
+  templateUrl: "./new-account.component.html",
+  styleUrls: ["./new-account.component.css"],
+  providers: [LogginService],
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
+
+  constructor(private logginService: LogginService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
-      status: accountStatus
+      status: accountStatus,
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+
+    this.logginService.logStatusChange(accountStatus);
   }
 }
